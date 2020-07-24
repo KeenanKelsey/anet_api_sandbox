aee43f9039bdc93e4858e66f544be0bb748744e6
TimesofLA <keenan.kelsey@gmail.com>
2020-07-24 15:56:57 -0600

    removed function from trans_cim_create.py

--- trans_cim_create.py
+++ trans_cim_create.py
@@ -166,69 +166,6 @@ def charge_credit_card(amount,save_to_cim=False):
     return response
 
 
-def get_transaction_list():
-    """get transaction list for a specific batch"""
-    merchantAuth = apicontractsv1.merchantAuthenticationType()
-    merchantAuth.name = CONSTANTS.apiLoginId
-    merchantAuth.transactionKey = CONSTANTS.transactionKey
-
-    # set sorting parameters
-    sorting = apicontractsv1.TransactionListSorting()
-    sorting.orderBy = apicontractsv1.TransactionListOrderFieldEnum.id
-    sorting.orderDescending = True
-
-    # set paging and offset parameters
-    paging = apicontractsv1.Paging()
-    # Paging limit can be up to 1000 for this request
-    paging.limit = 700
-    paging.offset = 1
-
-    transactionListRequest = apicontractsv1.getTransactionListRequest()
-    transactionListRequest.merchantAuthentication = merchantAuth
-    transactionListRequest.refId = "Sample"
-    transactionListRequest.batchId = "10728865" #Batch from 07/22/2020
-    transactionListRequest.sorting = sorting
-    transactionListRequest.paging = paging
-
-    transactionListController = getTransactionListController(transactionListRequest)
-    transactionListController.execute()
-
-    # Work on the response
-    response = transactionListController.getresponse()
-
-    if response is not None:
-        if response.messages.resultCode == apicontractsv1.messageTypeEnum.Ok:
-            if hasattr(response, 'transactions'):
-                print('Successfully retrieved transaction list.')
-                if response.messages is not None:
-                    print('Message Code: %s' % response.messages.message[0]['code'].text)
-                    print('Message Text: %s' % response.messages.message[0]['text'].text)
-                    print('Total Number In Results: %s' % response.totalNumInResultSet)
-                    print()
-                for transaction in response.transactions.transaction:
-                    print('Transaction Id: %s' % transaction.transId)
-                    print('Transaction Status: %s' % transaction.transactionStatus)
-                    if hasattr(transaction, 'accountType'):
-                        print('Account Type: %s' % transaction.accountType)
-                    print('Settle Amount: %.2f' % transaction.settleAmount)
-                    if hasattr(transaction, 'profile'):
-                        print('Customer Profile ID: %s' % transaction.profile.customerProfileId)
-                    print()
-            else:
-                if response.messages is not None:
-                    print('Failed to get transaction list.')
-                    print('Code: %s' % (response.messages.message[0]['code'].text))
-                    print('Text: %s' % (response.messages.message[0]['text'].text))
-        else:
-            if response.messages is not None:
-                print('Failed to get transaction list.')
-                print('Code: %s' % (response.messages.message[0]['code'].text))
-                print('Text: %s' % (response.messages.message[0]['text'].text))
-    else:
-        print('Error. No response received.')
-
-    return response
-
 def create_customer_profile_from_transaction(transactionId):
     merchantAuth = apicontractsv1.merchantAuthenticationType()
     merchantAuth.name = CONSTANTS.apiLoginId
@@ -262,4 +199,3 @@ def create_customer_profile_from_transaction(transactionId):
 
 if (os.path.basename(__file__) == os.path.basename(sys.argv[0])):
         charge_credit_card(CONSTANTS.amount)
-        
